b57eb6619922b531b1630a650f97ab6f633ec851
Arkansas Tom Jones <atte.kuosmanen@edu.savonia.fi>
2018-03-04 06:29:20 +0200

    Add files via upload
    Changed callback to event

--- esx_anticombatlog/sv_main.lua
+++ esx_anticombatlog/sv_main.lua
@@ -6,11 +6,17 @@
 
 DeadPlayers = {}
 
+function dbg(msg)
+	Citizen.Trace(tostring(msg) .. '\n')
+end
+
 RegisterServerEvent('esx_anticombatlog:request')
 AddEventHandler('esx_anticombatlog:request', function(id)
 	if DeadPlayers[id] ~= nil then
+		dbg('Player ' .. id .. ' combatlogged')
 		TriggerClientEvent('esx_anticombatlog:startPenalty', source)
 	else
+		dbg('Player ' .. id .. ' had not combatlogged')
 		TriggerClientEvent('esx_anticombatlog:allOk', source)
 	end
 end)
@@ -18,8 +24,10 @@ end)
 RegisterServerEvent('esx_anticombatlog:update')
 AddEventHandler('esx_anticombatlog:update', function(isDead, id)
 	if isDead then
+		dbg('Player ' .. id .. ' is dead')
 		DeadPlayers[id] = true
 	else
+		dbg('Player ' .. id .. ' is alive')
 		DeadPlayers[id] = nil
 	end
 end)
